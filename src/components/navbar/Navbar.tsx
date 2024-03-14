2db241fb21d614d88e448a0033dbd9b709e13d9c
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-14 18:15:53 +0900

    Pass session to children

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 import React from "react";
 import Links from "./links/Links";
 import styles from "./navbar.module.css";
+import { auth } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const userSession = await auth();
+  console.log(userSession);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>
         <Link href='/'>Korean Dictionary</Link>
       </h1>
       <div>
-        <Links />
+        <Links userSession={userSession} />
       </div>
     </div>
   );
