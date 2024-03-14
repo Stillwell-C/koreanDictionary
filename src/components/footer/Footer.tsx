4980188f24304ab6afdf897ce2157c0b6c66ec67
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-14 11:24:41 +0900

    Add name & link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.information}>
+        <span>Stillwell-C</span>
+        <span> | </span>
+        <a
+          href='https://github.com/Stillwell-C'
+          target='_blank'
+          aria-label='github'
+        >
+          <Image src='/github.svg' width='20' height='20' alt='' />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
