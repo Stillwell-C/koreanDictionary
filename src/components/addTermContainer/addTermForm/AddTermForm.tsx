e41ef8dc175670e8c2c06ddc005f928dfd57d6c9
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-15 18:37:01 +0900

    Add user message

--- src/components/addTermContainer/addTermForm/AddTermForm.tsx
+++ src/components/addTermContainer/addTermForm/AddTermForm.tsx
@@ -3,8 +3,9 @@
 import { addTermToList } from "@/lib/action";
 import { Session } from "next-auth";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useFormState } from "react-dom";
+import styles from "./addTermForm.module.css";
 
 type Props = {
   targetCode: string;
@@ -13,18 +14,53 @@ type Props = {
 
 const AddTermForm = ({ targetCode, userSession }: Props) => {
   const router = useRouter();
+  const messageRef = useRef<HTMLParagraphElement>(null);
 
   const [state, formAction] = useFormState(addTermToList, null);
+  const [displayMessage, setDisplayMessage] = useState(false);
 
   const unauthorizedUserButton = (
-    <button onClick={() => router.push("/login")}>Save Term</button>
+    <button className={styles.button} onClick={() => router.push("/login")}>
+      Save Term
+    </button>
   );
 
+  useEffect(() => {
+    if (state?.error || state?.success) {
+      setDisplayMessage(true);
+      messageRef?.current?.focus();
+      const displayTimeout = setTimeout(() => {
+        setDisplayMessage(false);
+      }, 5000);
+
+      return () => clearTimeout(displayTimeout);
+    }
+  }, [state]);
+
+  const message = state?.success
+    ? "Added to List"
+    : state?.errorMsg || "Error. Try again.";
+
   const addTermForm = (
-    <form action={formAction}>
-      <input type='text' value={targetCode} name='targetCode' hidden />
-      <input type='text' value={userSession?.user?.id} name='userId' hidden />
-      <button type='submit'>Save Term</button>
+    <form className={styles.form} action={formAction}>
+      <input type='text' value={targetCode} name='targetCode' hidden readOnly />
+      <input
+        type='text'
+        value={userSession?.user?.id}
+        name='userId'
+        hidden
+        readOnly
+      />
+      <button className={styles.button} disabled={state?.success} type='submit'>
+        {state?.success ? "Saved" : "Save Term"}
+      </button>
+      <div
+        className={`${styles.message} ${
+          displayMessage ? styles.displayMessage : ""
+        }`}
+      >
+        <p ref={messageRef}>{message}</p>
+      </div>
     </form>
   );
 
