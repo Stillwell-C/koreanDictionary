f6d7bb6a413b4b8148ee4d8e8389783ed3fe939b
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-15 09:31:35 +0900

    Add heading

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -6,6 +6,7 @@ const RegisterPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.formWrapper}>
+        <h2>Register</h2>
         <RegisterForm />
       </div>
     </div>
