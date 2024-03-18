67456b53e3cbc6dfd136b3bae0af9513e03cacac
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-18 13:06:44 +0900

    Initial commit

---
+++ src/components/termList/TermList.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import styles from "./termList.module.css";
+
+type Props = {
+  collectionId: string;
+  name: string;
+};
+
+const TermList = ({ collectionId, name }: Props) => {
+  const [languagePreference, setLanguagePreference] = useState("");
+
+  useEffect(() => {
+    const languagePreference = localStorage.getItem("langPreference");
+    if (languagePreference) setLanguagePreference(languagePreference);
+  }, []);
+
+  const collectionLink =
+    languagePreference !== "0" && languagePreference !== ""
+      ? `/userpage/collection/${collectionId}?translation=true&transLang=${languagePreference}`
+      : `/userpage/collection/${collectionId}`;
+
+  return (
+    <Link className={styles.collection} href={collectionLink}>
+      {name}
+    </Link>
+  );
+};
+
+export default TermList;
