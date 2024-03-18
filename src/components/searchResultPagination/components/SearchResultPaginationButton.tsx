4163063a56b3a1c8ff1762679d507fd26bd9ed88
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-18 17:11:27 +0900

    Add links to urls

--- src/components/searchResultPagination/components/SearchResultPaginationButton.tsx
+++ src/components/searchResultPagination/components/SearchResultPaginationButton.tsx
@@ -1,28 +1,38 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {
   pageNum: number;
   currentPageNum: number;
+  currentURL: string;
 };
 
-const SearchResultPaginationButton = ({ pageNum, currentPageNum }: Props) => {
+const SearchResultPaginationButton = ({
+  pageNum,
+  currentPageNum,
+  currentURL,
+}: Props) => {
+  const linkURL = pageNum <= 1 ? currentURL : `${currentURL}&start=${pageNum}`;
+
   return (
-    <button
-      type='button'
-      className={`posts-pagination-btn basic-button ${
-        currentPageNum === pageNum && "highlighted-btn"
-      }`}
-      disabled={currentPageNum === pageNum ? true : false}
-      aria-current={currentPageNum === pageNum ? true : false}
-      aria-label={
-        currentPageNum === pageNum
-          ? `Current Page, Page ${pageNum}`
-          : `Page ${pageNum}`
-      }
-      //   onClick={() => setCurrentPage(pageNum)}
-    >
-      {pageNum}
-    </button>
+    <Link href={linkURL}>
+      <button
+        type='button'
+        className={`posts-pagination-btn basic-button ${
+          currentPageNum === pageNum && "highlighted-btn"
+        }`}
+        disabled={currentPageNum === pageNum ? true : false}
+        aria-current={currentPageNum === pageNum ? true : false}
+        aria-label={
+          currentPageNum === pageNum
+            ? `Current Page, Page ${pageNum}`
+            : `Page ${pageNum}`
+        }
+        //   onClick={() => setCurrentPage(pageNum)}
+      >
+        {pageNum}
+      </button>
+    </Link>
   );
 };
 
