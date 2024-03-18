291a059b84afe8f8a70410ff939b98fc0c6f58d0
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-18 13:06:51 +0900

    Initial commit

---
+++ src/components/termListResult/TermListResult.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "./termListResult.module.css";
+import { getTermData } from "@/lib/apiData";
+import Link from "next/link";
+import RemoveTermContainer from "../removeTermContainer/RemoveTermContainer";
+
+type Props = {
+  resultData: {
+    _id: string;
+    targetCode: string;
+  };
+  translation?: string;
+  transLang?: string;
+  collectionId: string;
+};
+
+const TermListResult = async ({
+  resultData,
+  translation,
+  transLang,
+  collectionId,
+}: Props) => {
+  const data = await getTermData(
+    resultData?.targetCode,
+    translation,
+    transLang
+  );
+
+  const termLink =
+    translation === "true"
+      ? `/term/${resultData.targetCode}?translation=true&transLang=${transLang}`
+      : `/term/${resultData.targetCode}`;
+
+  return (
+    <div>
+      <div className={styles.topLine}>
+        <Link href={termLink}>
+          <p className={styles.entryTerm}>{data.word}</p>
+        </Link>
+        {data?.originalLanguage?.originalLanguage && (
+          <span>({data?.originalLanguage?.originalLanguage})</span>
+        )}
+        {data?.pos && <span>「{data?.pos}」</span>}
+        {data?.pronunciationInfo?.pronunciation && (
+          <span>[{data.pronunciationInfo.pronunciation}]</span>
+        )}
+        {data?.wordGrade && data?.wordGrade !== "없음" && (
+          <span>등급: {data?.wordGrade}</span>
+        )}
+        {/* <AddTermContainer targetCode={data.targetCode} /> */}
+        <RemoveTermContainer
+          targetCode={data.targetCode}
+          termCollectionId={collectionId}
+        />
+      </div>
+
+      <ol className={styles.list}>
+        {data.definitionAndExamples?.map((item) => (
+          <li key={item.definition}>
+            <p>{item?.translation?.transWord}</p>
+            <p>{item?.definition}</p>
+            <p>{item?.translation?.transDfn}</p>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default TermListResult;
