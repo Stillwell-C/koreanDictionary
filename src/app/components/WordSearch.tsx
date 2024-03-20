04f9031d10f670f191e3ae494aab541bdea5bdc0
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-20 18:22:58 +0900

    Comment out

--- src/app/components/WordSearch.tsx
+++ src/app/components/WordSearch.tsx
@@ -1,27 +1,27 @@
-import { getTermData, searchWord } from "@/lib/apiData";
-import Reciever from "./Reciever";
+// import { getTermData, searchWord } from "@/lib/apiData";
+// import Reciever from "./Reciever";
 
-const WordSearch = async () => {
-  // const data = await getTermData("23460", "true", "1");
-  const target_code = "23460";
-  const translation = "true";
-  const transLanguage = "1";
+// const WordSearch = async () => {
+//   // const data = await getTermData("23460", "true", "1");
+//   const target_code = "23460";
+//   const translation = "true";
+//   const transLanguage = "1";
 
-  const data = await fetch(
-    `https://krdict.korean.go.kr/api/view?key=${
-      process.env.API_KEY
-    }&method=target_code&q=${target_code}${
-      translation ? `&translated=y&trans_lang=${transLanguage}` : ""
-    }`
-  );
+//   const data = await fetch(
+//     `https://krdict.korean.go.kr/api/view?key=${
+//       process.env.API_KEY
+//     }&method=target_code&q=${target_code}${
+//       translation ? `&translated=y&trans_lang=${transLanguage}` : ""
+//     }`
+//   );
 
-  const text = await data.text();
+//   const text = await data.text();
 
-  return (
-    <div>
-      <Reciever data={text} />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <Reciever data={text} />
+//     </div>
+//   );
+// };
 
-export default WordSearch;
+// export default WordSearch;
