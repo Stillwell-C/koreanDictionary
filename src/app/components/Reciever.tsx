abd434eac5f541533df7605194a61055a673ba18
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-20 17:11:26 +0900

    Update to show new data

--- src/app/components/Reciever.tsx
+++ src/app/components/Reciever.tsx
@@ -3,66 +3,68 @@ import { parse } from "node-html-parser";
 import * as cheerio from "cheerio";
 
 type Props = {
-  data: TermDataResult;
+  data: string;
 };
 
 const Reciever = ({ data }: Props) => {
-  console.log(data);
+  // console.log(data);
   // const parsedData = new DOMParser().parseFromString(html, "text/xml");
   // console.log(parsedData);
 
-  // const $ = cheerio.load(html, { xmlMode: true });
-  // const targetCode = $("target_code").text();
-  // const wordInfo = $("word_info");
-  // const word = $(wordInfo).find("word").first().text();
-  // const wordUnit = $(wordInfo).find("word_unit").text();
-  // const pos = $(wordInfo).find("pos").first().text();
-  // const wordType = $(wordInfo).find("word_type").text();
-  // const wordGrade = $(wordInfo).find("word_grade").text();
-  // const originalLanguage = {
-  //   originalLanguage: $(wordInfo)
-  //     .find("original_language_info > original_language")
-  //     .text(),
-  //   originalLanguageType: $(wordInfo)
-  //     .find("original_language_info > language_type")
-  //     .text(),
-  // };
-  // const pronunciationInfo = {
-  //   pronunciation: $(wordInfo)
-  //     .find("pronunciation_info > pronunciation")
-  //     .text(),
-  //   pronunciationLink: $(wordInfo).find("pronunciation_info > link").text(),
-  // };
-  // const derivatives = $(wordInfo).find("der_info");
-  // const derivativesData = [];
-  // derivatives.each((i, item) => {
-  //   const word = $(item).find("word").text();
-  //   const targetCode = $(item).find("link_target_code").text();
-  //   const link = $(item).find("link").text();
-  //   derivativesData.push({ word, targetCode, link });
-  // });
-  // const categoryInfo = {
-  //   type: $(wordInfo).find("category_info > type").text(),
-  //   written_form: $(wordInfo).find("category_info > written_form").text(),
-  // };
-  // const senseInfo = $(wordInfo).find("sense_info");
-  // const definitionAndExamples = [];
-  // senseInfo.each((i, item) => {
-  //   const definition = $(item).find("definition").text();
-  //   const translation = {
-  //     transLang: $(item).find("translation > trans_lang").text().trim(),
-  //     transWord: $(item).find("translation > trans_word").text(),
-  //     transDfn: $(item).find("translation > trans_dfn").text(),
-  //   };
-  //   const examples = [];
-  //   const exampleInfo = $(item).find("example_info");
-  //   exampleInfo.each((i, item) => {
-  //     const type = $(item).find("type").text();
-  //     const example = $(item).find("example").text();
-  //     examples.push({ type, example });
-  //   });
-  //   definitionAndExamples.push({ definition, translation, examples });
-  // });
+  const $ = cheerio.load(data, { xmlMode: true });
+  const targetCode = $("target_code").text();
+  const wordInfo = $("word_info");
+  const word = $(wordInfo).find("word").first().text();
+  const wordUnit = $(wordInfo).find("word_unit").text();
+  const pos = $(wordInfo).find("pos").first().text();
+  const wordType = $(wordInfo).find("word_type").text();
+  const wordGrade = $(wordInfo).find("word_grade").text();
+  const originalLanguage = {
+    originalLanguage: $(wordInfo)
+      .find("original_language_info > original_language")
+      .text(),
+    originalLanguageType: $(wordInfo)
+      .find("original_language_info > language_type")
+      .text(),
+  };
+  const conju_info = $(wordInfo).find("conju_info");
+  console.log(conju_info);
+  const pronunciationInfo = {
+    pronunciation: $(wordInfo)
+      .find("pronunciation_info > pronunciation")
+      .text(),
+    pronunciationLink: $(wordInfo).find("pronunciation_info > link").text(),
+  };
+  const derivatives = $(wordInfo).find("der_info");
+  const derivativesData = [];
+  derivatives.each((i, item) => {
+    const word = $(item).find("word").text();
+    const targetCode = $(item).find("link_target_code").text();
+    const link = $(item).find("link").text();
+    derivativesData.push({ word, targetCode, link });
+  });
+  const categoryInfo = {
+    type: $(wordInfo).find("category_info > type").text(),
+    written_form: $(wordInfo).find("category_info > written_form").text(),
+  };
+  const senseInfo = $(wordInfo).find("sense_info");
+  const definitionAndExamples = [];
+  senseInfo.each((i, item) => {
+    const definition = $(item).find("definition").text();
+    const translation = {
+      transLang: $(item).find("translation > trans_lang").text().trim(),
+      transWord: $(item).find("translation > trans_word").text(),
+      transDfn: $(item).find("translation > trans_dfn").text(),
+    };
+    const examples = [];
+    const exampleInfo = $(item).find("example_info");
+    exampleInfo.each((i, item) => {
+      const type = $(item).find("type").text();
+      const example = $(item).find("example").text();
+      examples.push({ type, example });
+    });
+    definitionAndExamples.push({ definition, translation, examples });
+  });
 
   // console.log({
   //   targetCode,
--- src/app/components/WordSearch.tsx
+++ src/app/components/WordSearch.tsx
@@ -2,11 +2,24 @@ import { getTermData, searchWord } from "@/lib/apiData";
 import Reciever from "./Reciever";
 
 const WordSearch = async () => {
-  const data = await getTermData("23460", "true", "1");
+  // const data = await getTermData("23460", "true", "1");
+  const target_code = "23460";
+  const translation = "true";
+  const transLanguage = "1";
+
+  const data = await fetch(
+    `https://krdict.korean.go.kr/api/view?key=${
+      process.env.API_KEY
+    }&method=target_code&q=${target_code}${
+      translation ? `&translated=y&trans_lang=${transLanguage}` : ""
+    }`
+  );
+
+  const text = await data.text();
 
   return (
     <div>
-      <Reciever data={data} />
+      <Reciever data={text} />
     </div>
   );
 };
