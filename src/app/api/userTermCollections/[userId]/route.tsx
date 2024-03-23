068ca4dfdb2ad53a9140fe4c2925bd229008e982
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-23 17:22:53 +0900

    initial commit

---
+++ src/app/api/userTermCollections/[userId]/route.tsx
@@ -0,0 +1,26 @@
+import { getTermCollections } from "@/lib/dbData";
+import { NextRequest, NextResponse } from "next/server";
+
+type Params = {
+  params: {
+    userId: string;
+  };
+};
+
+export const GET = async (
+  request: NextRequest,
+  { params: { userId } }: Params
+) => {
+  const searchParams = request.nextUrl.searchParams;
+  const start = searchParams.get("start") || "1";
+  const results = searchParams.get("results") || "10";
+  console.log(searchParams);
+
+  if (!userId) {
+    throw new Error("Unauthorized request. Please log in.");
+  }
+
+  const termCollections = await getTermCollections(userId, start, results);
+
+  return NextResponse.json(termCollections.results);
+};
