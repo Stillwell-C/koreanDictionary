b608e52c4e3cae39a5acc3e1d485273b70a9c3bd
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-23 15:29:14 +0900

    Convert to button

--- src/components/searchResultPagination/components/SearchResultPaginationButton.tsx
+++ src/components/searchResultPagination/components/SearchResultPaginationButton.tsx
@@ -1,38 +1,43 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 type Props = {
   pageNum: number;
   currentPageNum: number;
-  currentURL: string;
+  pageUrlGenerator: (page: number) => string;
 };
 
 const SearchResultPaginationButton = ({
   pageNum,
   currentPageNum,
-  currentURL,
+  pageUrlGenerator,
 }: Props) => {
-  const linkURL = pageNum <= 1 ? currentURL : `${currentURL}&start=${pageNum}`;
+  const router = useRouter();
+
+  const handleRoute = () => {
+    const url = pageUrlGenerator(pageNum);
+    router.push(url);
+  };
 
   return (
-    <Link href={linkURL}>
-      <button
-        type='button'
-        className={`posts-pagination-btn basic-button ${
-          currentPageNum === pageNum && "highlighted-btn"
-        }`}
-        disabled={currentPageNum === pageNum ? true : false}
-        aria-current={currentPageNum === pageNum ? true : false}
-        aria-label={
-          currentPageNum === pageNum
-            ? `Current Page, Page ${pageNum}`
-            : `Page ${pageNum}`
-        }
-        //   onClick={() => setCurrentPage(pageNum)}
-      >
-        {pageNum}
-      </button>
-    </Link>
+    <button
+      type='button'
+      className={`posts-pagination-btn basic-button ${
+        currentPageNum === pageNum && "highlighted-btn"
+      }`}
+      disabled={currentPageNum === pageNum ? true : false}
+      aria-current={currentPageNum === pageNum ? true : false}
+      aria-label={
+        currentPageNum === pageNum
+          ? `Current Page, Page ${pageNum}`
+          : `Page ${pageNum}`
+      }
+      onClick={handleRoute}
+    >
+      {pageNum}
+    </button>
   );
 };
 
