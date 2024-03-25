6d964c6162b5efaaa9c4904076463b9433ea6536
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-25 12:48:08 +0900

    Add image and text

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Links from "./links/Links";
 import styles from "./navbar.module.css";
 import { auth } from "@/lib/auth";
+import Image from "next/image";
 
 const Navbar = async () => {
   const userSession = await auth();
@@ -10,9 +11,25 @@ const Navbar = async () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>
-        <Link href='/'>Korean Dictionary</Link>
-      </h1>
+      <div>
+        <Link className={styles.headerLink} href='/'>
+          <div className={styles.imgDiv}>
+            <Image src='./Taegeuk.svg' alt='' height='40' width='40' />
+          </div>
+          <div className={styles.headingText}>
+            <h1 className={styles.heading}>
+              <span>꼬</span>
+              <span>박</span>
+              <span>꼬</span>
+              <span>박</span>
+            </h1>
+
+            <div className={styles.subheading}>
+              <span>사전 & 플래시카드</span>
+            </div>
+          </div>
+        </Link>
+      </div>
       <div>
         <Links userSession={userSession} />
       </div>
