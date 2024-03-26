ffaa723545339a4bf223cd6448bb5b8fae08585f
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-03-26 14:32:12 +0900

    Change layout

--- src/components/termListResult/TermListResult.tsx
+++ src/components/termListResult/TermListResult.tsx
@@ -34,24 +34,35 @@ const TermListResult = async ({
   return (
     <div>
       <div className={styles.topLine}>
-        <Link href={termLink}>
-          <p className={styles.entryTerm}>{data.word}</p>
-        </Link>
-        {data?.originalLanguage?.originalLanguage && (
-          <span>({data?.originalLanguage?.originalLanguage})</span>
-        )}
-        {data?.pos && <span>「{data?.pos}」</span>}
-        {data?.pronunciationInfo?.pronunciation && (
-          <span>[{data.pronunciationInfo.pronunciation}]</span>
-        )}
-        {data?.wordGrade && data?.wordGrade !== "없음" && (
-          <span>등급: {data?.wordGrade}</span>
-        )}
-        {/* <AddTermContainer targetCode={data.targetCode} /> */}
-        <RemoveTermContainer
-          targetCode={data.targetCode}
-          termCollectionId={collectionId}
-        />
+        <div className={styles.wordLine}>
+          <Link href={termLink}>
+            <p className={styles.entryTerm}>{data.word}</p>
+          </Link>
+          <div className={styles.rmvTermMobile}>
+            <RemoveTermContainer
+              targetCode={data.targetCode}
+              termCollectionId={collectionId}
+            />
+          </div>
+        </div>
+        <div className={styles.wordData}>
+          {data?.originalLanguage?.originalLanguage && (
+            <span>({data?.originalLanguage?.originalLanguage})</span>
+          )}
+          {data?.pos && <span>「{data?.pos}」</span>}
+          {data?.pronunciationInfo?.pronunciation && (
+            <span>[{data.pronunciationInfo.pronunciation}]</span>
+          )}
+          {data?.wordGrade && data?.wordGrade !== "없음" && (
+            <span>등급: {data?.wordGrade}</span>
+          )}
+          <div className={styles.rmvTermDesktop}>
+            <RemoveTermContainer
+              targetCode={data.targetCode}
+              termCollectionId={collectionId}
+            />
+          </div>
+        </div>
       </div>
 
       <ol className={styles.list}>
