69b49170965f26b0473f72a16feb1054c2e83f84
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-04-12 16:14:29 +0900

    Improve range of POS for bold presentation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,39 @@ export const organizeExamples = (
   const dialog = [];
 
   const formatSentence = (sentence: string) => {
-    if (wordInfo?.pos === "명사" || wordInfo?.pos === "관형사") {
+    if (wordInfo?.pos === "접사") {
+      return `<strong>${sentence}</strong>`;
+    }
+
+    if (wordInfo?.pos === "조사" || wordInfo?.pos === "어미") {
+      const parsedWord = wordInfo.word.replace(/-ㄴ|-ㄹ|-으|-/, "");
+
+      //Add return for dialog sentences not containing word
+      if (!sentence.includes(parsedWord)) return sentence;
+
+      //Create regex for whole word from determined root
+      const wordRegex = new RegExp(
+        `([\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+${parsedWord})`
+      );
+
+      //Check for term & return for sentences not containing word
+      const wordInSentence = sentence.match(wordRegex);
+      if (!wordInSentence?.length) return sentence;
+
+      const location = sentence.split(wordInSentence[0]);
+      return `<span>${location[0]}</span> <strong>${wordInSentence}</strong><span>${location[1]}</span>`;
+    }
+
+    if (
+      wordInfo?.pos === "명사" ||
+      wordInfo?.pos === "관형사" ||
+      wordInfo?.pos === "부사" ||
+      wordInfo?.pos === "대명사" ||
+      wordInfo?.pos === "수사" ||
+      wordInfo?.pos === "관형사" ||
+      wordInfo?.pos === "감탄사" ||
+      wordInfo?.pos === "의존 명사"
+    ) {
       //Add return for dialog sentences not containing word
       if (!sentence.includes(wordInfo.word)) return sentence;
 
@@ -16,30 +48,24 @@ export const organizeExamples = (
     }
 
     //Find word root
-    if (wordInfo.origin === "한자고유어") {
+    if (
+      wordInfo?.pos === "형용사" ||
+      wordInfo?.pos === "동사" ||
+      wordInfo?.pos === "보조 형용사" ||
+      wordInfo?.pos === "보조 동사"
+    ) {
       let wordRoot = wordInfo.word;
+
       //Remove verb ending
-      const verbEndings = ["하다", "되다", "시키다"];
-      for (const ending of verbEndings) {
-        if (wordRoot.includes(ending))
-          wordRoot = wordRoot.slice(0, wordRoot.lastIndexOf(ending));
+      if (origin === "한자고유어") {
+        const verbEndings = ["하다", "되다", "시키다"];
+        for (const ending of verbEndings) {
+          if (wordRoot.includes(ending))
+            wordRoot = wordRoot.slice(0, wordRoot.lastIndexOf(ending));
+        }
+      } else {
+        wordRoot = wordInfo?.word.slice(0, wordInfo?.word.lastIndexOf("다"));
       }
-
-      //Create regex for whole word from determined root
-      const wordRegex = new RegExp(
-        `(${wordRoot}[\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)`
-      );
-
-      //Check for term & return for sentences not containing word
-      const wordInSentence = sentence.match(wordRegex);
-      if (!wordInSentence?.length) return sentence;
-
-      const location = sentence.split(wordInSentence[0]);
-      return `<span>${location[0]}</span> <strong>${wordInSentence[0]}</strong><span>${location[1]}</span>`;
-    }
-
-    if (wordInfo?.pos === "형용사" || wordInfo?.pos === "동사") {
-      let wordRoot = wordInfo.word.slice(0, wordInfo.word.lastIndexOf("다"));
 
       //Create regex for whole word from determined root
       const wordRegex = new RegExp(
