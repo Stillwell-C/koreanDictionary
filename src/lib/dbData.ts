6bcd92ecfd54f54f87e7ea105522e50d6f93857f
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-04-16 18:27:08 +0900

    Add functions to modify saved terms

--- src/lib/dbData.ts
+++ src/lib/dbData.ts
@@ -1,6 +1,7 @@
 import { connectDB } from "./dbUtils";
 import { FailedToFetchError } from "./exceptions";
 import { BlogPost, SavedTerm, TermCollection } from "./models";
+import { addIntervalToDate } from "./utils";
 
 export const getTermCollection = async (termCollectionId: string) => {
   connectDB();
@@ -128,6 +129,47 @@ export const getAllSavedTerms = async (termCollectionId: string) => {
   }
 };
 
+export const getSavedTermForStudy = async (
+  termCollectionId: string,
+  start: string
+) => {
+  connectDB();
+  const startNum = parseInt(start);
+  const skipNum = startNum - 1;
+
+  if (!termCollectionId) {
+    throw new Error("term collection ID must be provided");
+  }
+
+  try {
+    const savedTerms: SavedTermResponse[] = await SavedTerm.find({
+      termCollectionId,
+      completedForSession: false,
+    })
+      .limit(1)
+      .skip(skipNum)
+      .select("_id targetCode")
+      .sort("-createdAt")
+      .populate("termCollectionId", "name -_id");
+
+    const savedTermsTotal = await SavedTerm.countDocuments({
+      termCollectionId,
+      completedForSession: false,
+    });
+
+    return {
+      results: savedTerms[0],
+      searchData: {
+        total: savedTermsTotal.toString(),
+        start,
+        num: 1,
+      },
+    };
+  } catch (err) {
+    throw new FailedToFetchError();
+  }
+};
+
 export const createTermCollection = async (userId: string, name: string) => {
   if (!userId || !name) {
     throw new Error("Must provide a user ID and collection name.");
@@ -137,6 +179,104 @@ export const createTermCollection = async (userId: string, name: string) => {
     userId,
     name,
   });
+};
+
+export const updateSavedTerm = async (
+  term: SavedTermResponse,
+  responseQuality: number
+) => {
+  //Increase the number of times a flashcard has been viewed
+  term.repititions += 1;
+
+  //Create repition interval for n-th repetition in days
+  let interval;
+  if (term.repititions === 1) {
+    interval = 1;
+  } else if (term.repititions === 2) {
+    interval = 6;
+  } else {
+    //If interval is a fraction, round it up to the nearest integer.
+    interval = Math.ceil(term.interval * term.easiness);
+  }
+
+  //Calculate updated easiness
+  let updatedEasiness =
+    term.easiness +
+    (0.1 - (5 - responseQuality) * (0.08 + (5 - responseQuality) * 0.2));
+
+  //Min allowable easiness is 1.3
+  if (updatedEasiness < 1.3) {
+    updatedEasiness = 1.3;
+  }
+
+  //If response lower than 3, start repiritions from beginning without changing easiness
+  //Else modify the easiness to updated
+  if (responseQuality < 3) {
+    interval = 1;
+    term.repititions = 1;
+  } else {
+    term.easiness = updatedEasiness;
+  }
+
+  term.interval = interval;
+  //Update review date
+  term.nextReview = addIntervalToDate(interval);
+  term.completedForSession = responseQuality > 4;
+
+  //Update db
+  try {
+    connectDB();
+
+    await SavedTerm.findOneAndUpdate({ _id: term._id }, term);
+  } catch (err) {
+    throw new Error("Something went wrong");
+  }
+};
+
+export const checkStudySession = async (termCollectionId: string) => {
+  try {
+    connectDB();
+
+    const currentDate = new Date().getDate();
+    const { lastReview } = await TermCollection.findById(
+      termCollectionId
+    ).select("lastReview");
+
+    // if (currentDate - lastReview > 86400000) {
+    //   startNewStudySession(termCollectionId);
+    // }
+    startNewStudySession(termCollectionId);
+  } catch (err) {
+    throw new Error("Something went wrong");
+  }
+};
+
+export const startNewStudySession = async (termCollectionId: string) => {
+  try {
+    connectDB();
+
+    //update last review to current time
+    const currentDate = new Date();
+    await TermCollection.findOneAndUpdate(
+      { _id: termCollectionId },
+      { lastReview: currentDate }
+    );
+
+    //clear all terms from past sessions
+    await SavedTerm.updateMany(
+      { termCollectionId, completedForSession: false },
+      { completedForSession: true }
+    );
+
+    //Select terms for new session
+    //TODO: add way to select length for this
+    await SavedTerm.updateMany(
+      { termCollectionId },
+      { completedForSession: false }
+    );
+  } catch (err) {
+    throw new Error("Something went wrong");
+  }
 };
 
 //Get title, thumbnail, and id for each blog post
