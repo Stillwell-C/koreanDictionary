ffa1c485dd763a114d17a12de0335932ddded0de
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-08-26 14:31:11 +0900

    Remove code related to CSV & frequency

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,7 +6,6 @@ import {
 import { ChatOpenAI } from "@langchain/openai";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { HttpResponseOutputParser } from "langchain/output_parsers";
-import { CSVLoader } from "langchain/document_loaders/fs/csv";
 
 export const dynamic = "force-dynamic";
 
@@ -29,10 +28,6 @@ Current conversation: {chat_history}
 user: {question}
 assistant:`;
 
-// If the user asks about the frequency of a word, you can refer to the frequency context. Here, the first column 차례 shows the frequency rank of the word, and the second column 항목 shows the word. There are multiple definitions for words, so you can ignore the numbers next to words and just give the highest rank for any identical word. Do not refer to this document for questions about topics other than word frequency.
-
-// Frequency context: {frequencyContext}
-
 const formatMessage = (message: ChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
@@ -42,10 +37,6 @@ export const POST = async (req: Request) => {
     const { messages, sentenceQuery, translatedSentence } = await req.json();
     const formattedMessageHistory = messages.slice(0, -1).map(formatMessage);
     const currentMessageContent = messages[messages.length - 1].content;
-
-    // const loader = new CSVLoader("src/app/data/Korean Frequency - 단어.csv", );
-
-    // const docs = await loader.load();
 
     const prompt = PromptTemplate.fromTemplate(template);
 
@@ -66,7 +57,6 @@ export const POST = async (req: Request) => {
       question: currentMessageContent,
       sentenceQuery: sentenceQuery,
       translatedSentence: translatedSentence,
-      // frequencyContext: docs.toString(),
     });
 
     // Respond with the stream
